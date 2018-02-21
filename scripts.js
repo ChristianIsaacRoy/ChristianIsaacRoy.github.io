8b2acea89cdbc46e555b777d22b59192a3bd8ff7
Christian <christian.50.roy@gmail.com>
2018-02-21 13:38:47 -0700

    Update ChefIt, mobile friendly, jquery accordions, update text

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
-// $("#gbimg").hover(function() {
-//     //fadeout first image using jQuery fadeOut
-//     $("#img1").fadeOut(0);
-//     //fadein second image using jQuery fadeIn 
-//     $("#img2").fadeIn(500);
-// }, function () {
-//     //fadeout second image using jQuery fadeOut
-//     $("#img1").fadeIn(0);
-//     //fadein first image using jQuery fadeIn
-//     $("#img2").fadeOut(500);
-// });
+$( ".snippet" ).accordion({
+    collapsible: true,
+    active: false
+});
