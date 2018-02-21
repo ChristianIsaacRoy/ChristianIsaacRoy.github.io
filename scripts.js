106c08c4205fc5655884399cd92e92e4909aed7a
Christian <christian.50.roy@gmail.com>
2018-02-20 22:15:59 -0700

    image move

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// $("#gbimg").hover(function() {
+//     //fadeout first image using jQuery fadeOut
+//     $("#img1").fadeOut(0);
+//     //fadein second image using jQuery fadeIn 
+//     $("#img2").fadeIn(500);
+// }, function () {
+//     //fadeout second image using jQuery fadeOut
+//     $("#img1").fadeIn(0);
+//     //fadein first image using jQuery fadeIn
+//     $("#img2").fadeOut(500);
+// });
